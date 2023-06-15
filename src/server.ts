e7c2ac8c317e313f9816d0d01cb07da2aefb4e29
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-06-14 21:46:23 -0300

    chore: folder structuring

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,7 @@
-import fastify from 'fastify'
-
+import { app } from './app'
 import { env } from './env'
 
-const app = fastify()
-
 const { PORT } = env
-
-app.get('/', async (request, reply) => {
-  return { hello: 'world' }
-})
 
 app
   .listen({
