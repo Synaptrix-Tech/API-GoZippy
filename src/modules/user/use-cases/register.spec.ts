d3c704c7a931ce7ce71b18b945337b617c9df48a
Felipe Bastos <felipe.bastos@kaber.com.br>
2023-06-15 11:59:17 -0300

    feat: adding hash for password and creating test

--- src/modules/user/use-cases/register.spec.ts
+++ src/modules/user/use-cases/register.spec.ts
@@ -1,6 +1,7 @@
 import { expect, describe, it, beforeEach } from 'vitest'
 import { RegisterUseCase } from './register'
 import { InMemoryUsersRepository } from '../repositories/in-memory/in-memory-users-repository'
+import { compare } from 'bcryptjs'
 
 let usersRepository: InMemoryUsersRepository
 let sut: RegisterUseCase
@@ -18,8 +19,18 @@ describe('Register Use Case', () => {
       password: '123456',
     })
 
-    console.log(user)
-
     expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should hash user password upon registration', async () => {
+    const { user } = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare('123456', user.password)
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
   })
 })
--- src/modules/user/use-cases/register.ts
+++ src/modules/user/use-cases/register.ts
@@ -1,4 +1,6 @@
 import { User } from '@prisma/client'
+import { hash } from 'bcryptjs'
+
 import { IUsersRepository } from '../repositories/IUsersRepository'
 
 interface IRegisterUseCaseRequest {
@@ -19,10 +21,12 @@ export class RegisterUseCase {
     email,
     password,
   }: IRegisterUseCaseRequest): Promise<IRegisterUseCaseResponse> {
+    const passwordHash = await hash(password, 8)
+
     const user = await this.usersRepository.create({
       name,
       email,
-      password,
+      password: passwordHash,
     })
 
     return { user }
