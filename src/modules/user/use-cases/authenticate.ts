02dabc638310db6def5f61af84f46191d76bc745
Felipe Bastos <felipe.bastos@kaber.com.br>
2023-06-15 14:33:58 -0300

    feat: creating use case for authenticate

---
+++ src/modules/user/use-cases/authenticate.ts
@@ -0,0 +1,39 @@
+import { compare } from 'bcryptjs'
+import { User } from '@prisma/client'
+
+import { IUsersRepository } from '../repositories/IUsersRepository'
+import { InvalidCredentialsError } from '../errors/invalid-credentials-error'
+
+interface IAuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface IAuthenticateUseCaseResponse {
+  user: User
+}
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: IAuthenticateUseCaseRequest): Promise<IAuthenticateUseCaseResponse> {
+    const user = await this.usersRepository.findUserByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, user.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
