c51403df011efa0f193e6ca2d8f0ef71778116be
Felipe da Costa <felipe.bastos.dev@gmail.com>
2023-06-16 10:14:53 -0300

    feat: creating flow for user authentication (#12)

---
+++ src/http/controllers/user/authenticate.ts
@@ -0,0 +1,36 @@
+import { InvalidCredentialsError } from '@/modules/user/errors/invalid-credentials-error'
+import { makeAuthenticateUseCase } from '@/modules/user/factories/make-authenticate-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(request.body)
+
+  try {
+    const authenticateUseCase = makeAuthenticateUseCase()
+
+    const { user } = await authenticateUseCase.execute({
+      email,
+      password,
+    })
+
+    return reply.status(200).send({
+      ...user,
+      password_hash: undefined,
+    })
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: err.message })
+    }
+
+    throw err
+  }
+}
