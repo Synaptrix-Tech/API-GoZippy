96113f2d4e947500b221311d37f367af67c6584e
Felipe da Costa <felipe.bastos.dev@gmail.com>
2023-06-23 09:21:22 -0300

    Feature/creating api docs (#22)

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,30 @@
 import fastify from 'fastify'
 import { ZodError } from 'zod'
+import fastifySwagger from '@fastify/swagger'
+import swaggerUI from '@fastify/swagger-ui'
+import path from 'node:path'
 
 import { env } from './env'
 import { usersRoutes } from './http/controllers/user/routes'
 import { driversRoutes } from './http/controllers/driver/routes'
 
 export const app = fastify()
+
+const pathOpenApi = path.join(__dirname, 'docs', 'openapi.json')
+
+app.register(fastifySwagger, {
+  mode: 'static',
+  specification: {
+    path: pathOpenApi,
+    baseDir: path.join(__dirname, 'docs'),
+  },
+})
+
+app.register(swaggerUI, {
+  baseDir: path.join(__dirname, 'docs'),
+  routePrefix: '/docs',
+  staticCSP: true,
+})
 
 app.register(usersRoutes, {
   prefix: '/users',
