5165e93f153fcc741c95bf3901bab45340e2ec89
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-06-23 11:03:24 -0300

    feat: creating use case for get user profile

--- src/modules/user/repositories/IUsersRepository.ts
+++ src/modules/user/repositories/IUsersRepository.ts
@@ -2,5 +2,6 @@ import { Prisma, User } from '@prisma/client'
 
 export interface IUsersRepository {
   findUserByEmail(email: string): Promise<User | null>
+  findUserById(id: string): Promise<User | null>
   create(data: Prisma.UserCreateInput): Promise<User>
 }
